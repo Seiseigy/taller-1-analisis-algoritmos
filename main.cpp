bc6269e3a4a9685e116c58c721c13aa12e5b0403
Sebastian <sebastian.albornozm@utem.cl>
2020-04-25 21:39:51 -0400

    habia una plantilla por la chucha

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+void participantes();
+
 int main(int argc, char** argv){
     cout << "Chao, mundo"<< endl;
-    cout << argv[1] << endl;
-    return 0;
+    if(argc > 1){
+        cout << argv[1] << endl;
+    }
+    else{
+        participantes();
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+void participantes() {
+    std::cout << std::endl << "=== Taller 01 ===" << std::endl;
+    std::cout << std::endl << "Sebastian Albornoz"; // Reemplazar por su nombre
+    std::cout << std::endl << "Sebastian Santelices"; // Reemplazar por su nombre
+    std::cout << std::endl << "Jorge Verdugo" << std::endl; // Reemplazar por su nombre
 }
