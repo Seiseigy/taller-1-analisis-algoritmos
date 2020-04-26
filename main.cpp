6d5635e704cdeb208868d82827349a268e1d3b4e
Jorge Verdugo <jorgeverdugoch@gmail.com>
2020-04-26 15:17:21 -0400

    ¡Caracoles! Había una plantilla

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
-int main(int argc, char** argv){
-    cout << "Chao, mundo"<< endl;
-    cout << argv[1] << endl;
-    return 0;
+void participantes();
+
+int main(int argc, char** argv) {
+    cout << "Chao, mundo" << endl;
+    if (argc > 1) {
+        cout << argv[1] << endl;
+    } else {
+        participantes();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void participantes() {
+    cout << endl
+         << "=== Taller 01 ===" << endl;
+    cout << endl
+         << "Sebastian Albornoz";
+    cout << endl
+         << "Sebastian Santelices";
+    cout << endl
+         << "Jorge Verdugo" << endl;
 }
