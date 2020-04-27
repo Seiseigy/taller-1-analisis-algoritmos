e10f3d1c8fe60f69d5b5776f6f4542f3d784a450
Sebastian <sebastian.albornozm@utem.cl>
2020-04-26 21:57:44 -0400

    pregunta uno, falta orden pero funcional

--- main.cpp
+++ main.cpp
@@ -1,24 +1,71 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 void participantes();
 
-int main(int argc, char** argv){
-    cout << "Chao, mundo"<< endl;
-    if(argc > 1){
-        cout << argv[1] << endl;
+bool validarSimbolos(char caracter, char lenguaje[])
+{
+    for (size_t i = 0; i < strlen(lenguaje); i++)
+    {
+        if (caracter == lenguaje[i])
+        {
+            return true;
+        }
     }
-    else{
-        participantes();
-    }
-    
-    return EXIT_SUCCESS;
-}
+};
 
-void participantes() {
-    std::cout << std::endl << "=== Taller 01 ===" << std::endl;
-    std::cout << std::endl << "Sebastian Albornoz"; // Reemplazar por su nombre
-    std::cout << std::endl << "Sebastian Santelices"; // Reemplazar por su nombre
-    std::cout << std::endl << "Jorge Verdugo" << std::endl; // Reemplazar por su nombre
-}
+int main(int argc, char **argv)
+{
+    cout << "Chao, mundo" << endl;
+    char lenguaje[4] = {'a', 'b', 'c', '*'};
+
+    if (argc > 1)
+    {
+        string copia = argv[1];
+        if (copia[0] != 'a')
+        {
+            cout << "Palabra no valida, no comienza con la letra a" << endl;
+            return 99;
+        }
+        else
+        {
+            for (size_t i = 0; i < copia.length(); i++)
+            {
+                if (!validarSimbolos(copia[i], lenguaje))
+                {
+                    cout << "Caracter " << copia[i] << " NO pertenece al lenguaje" << endl;
+                    return 99;
+                }
+                else if (copia[i] == '*')
+                {
+                    cout << "Palabra no valida, contiene el caracter *" << endl;
+                    //verificar que las letras correspondan al lenguaje
+                    return 99;
+                }
+            }
+            cout << "Palabra valida"<< endl;
+            return EXIT_SUCCESS;
+        }
+    }
+    else
+    {
+        participantes();
+        return EXIT_SUCCESS;
+    }
+};
+
+void participantes()
+{
+    std::cout << std::endl
+              << "=== Taller 01 ===" << std::endl;
+    std::cout << std::endl
+              << "Sebastian Albornoz"; // Reemplazar por su nombre
+    std::cout << std::endl
+              << "Sebastian Santelices"; // Reemplazar por su nombre
+    std::cout << std::endl
+              << "Jorge Verdugo" << std::endl; // Reemplazar por su nombre
+};
